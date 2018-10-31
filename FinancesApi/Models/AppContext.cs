76677ebcfd98b7c5cf3fbf02717a874cefe4f6a2
Michael Smith <michaelasmith1991@gmail.com>
2018-10-31 15:22:38 +0000

    Added starting stuff

---
+++ FinancesApi/Models/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinancesApi.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions opts) : base(opts)
+        {
+        }
+        public DbSet<Income> Incomes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
--- FinancesApi/Startup.cs
+++ FinancesApi/Startup.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinancesApi.Models;
+using FinancesApi.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,6 +26,9 @@ namespace FinancesApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(opts =>
+              opts.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+            services.AddSingleton(typeof(IDataAccess<Income, int>), typeof(DataAccessRepository));
             services.AddMvc();
         }
 
